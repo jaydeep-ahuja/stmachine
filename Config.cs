bbbd86de520a1daff30d113377584d37fe3a6d6b
ashishjain.ash <ashishjain.ash@gmail.com>
2011-02-16 17:48:06 +0000

    Document Comments added

--- Config.cs
+++ Config.cs
@@ -6,13 +6,25 @@ using StateMachine.Rules.Interpreter;
 
 namespace StateMachine
 {
+    /// <summary>
+    /// <![CDATA[Config<T> class.]]> [Singleton class]
+    /// Used to list objects which would be accessed by other classes while performing the operation.
+    /// </summary>
+    /// <typeparam name="T">T specifies the type of input state machine will process</typeparam>
     public class Config<T>
     {
         private static Config<T> configuration;
         private Config() { }
 
+        /// <summary>
+        /// Custom comparer specified while creating the instance of State Machine
+        /// </summary>
         public ITransitionEventComparer stateMachineInputComparer;
 
+        /// <summary>
+        /// Method to get the instance of the Config class.
+        /// </summary>
+        /// <returns><![CDATA[Config<T> object.]]></returns>
         public static Config<T> GetInstance()
         {
             if (configuration == null)
--- State.cs
+++ State.cs
@@ -6,16 +6,28 @@ using StateMachine.Rules.Interpreter;
 
 namespace StateMachine
 {
+    /// <summary>
+    /// <![CDATA[State<T> entity class]]>
+    /// State class is used by state machine to represent a rule in rule file and to buffer the input processed by this state.
+    /// </summary>
+    /// <typeparam name="T">T specifies the type of input state machine will process</typeparam>
     public class State<T>
     {
         private IRule rule;
 
+        /// <summary>
+        /// Private State constructor
+        /// </summary>
+        /// <param name="rule">Rule object from which to create the state.</param>
         private State(IRule rule)
         {
             inputBuffer = new List<T>();
             this.rule = rule;
         }
 
+        /// <summary>
+        /// Name of the state as read from the rule
+        /// </summary>
         public String Name
         {
             get
@@ -24,15 +36,31 @@ namespace StateMachine
             }
             set { }
         }
+
+        /// <summary>
+        /// Buffer of input current state has processed.
+        /// Used by comparer to compare with events in rule file.
+        /// </summary>
         private List<T> inputBuffer;
 
+        /// <summary>
+        /// Method to create a state of a rule.
+        /// </summary>
+        /// <param name="rule">Rule object.</param>
+        /// <returns>Created State object.</returns>
         internal static State<T> Create(IRule rule)
         {
             return new State<T>(rule);
         }
 
-        // TODO: Currently State Machine on a single match
-        // Will need immediate change
+        /// <summary>
+        /// Method to process the input.
+        /// TODO: Currently State Machine on a single match
+        /// Will need immediate change
+        /// </summary>
+        /// <param name="interpreter">Rule Interpreter as provided by dependency injection.</param>
+        /// <param name="input">Input object to process</param>
+        /// <returns>Returns the State loaded after processing the input over the rule file.</returns>
         internal State<T> Process(IRuleInterpreter interpreter, T input)
         {
             inputBuffer.Add(input);
