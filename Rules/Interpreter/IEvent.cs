78f4bfc670f57fb0f7df60c0e89c572ac7202a63
ashishjain.ash <ashishjain.ash@gmail.com>
2011-02-22 16:50:24 +0000

    Build Version

---
+++ Rules/Interpreter/IEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StateMachine.Rules.Interpreter
+{
+    public interface IEvent
+    {
+        String Name { get; set; }
+        Object Context { get; set; }
+        Boolean IsTriggered { get; set; }
+    }
+}
---
+++ Rules/Interpreter/SCXML/SCXMLDataEvent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StateMachine.Rules.Interpreter.SCXML
+{
+    public class SCXMLDataEvent : IEvent
+    {
+        private String _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        private object _context;
+        public object Context
+        {
+            get
+            {
+                return _context;
+            }
+            set
+            {
+                _context = value;
+            }
+        }
+
+        private bool _isTriggred = false;
+        public bool IsTriggered
+        {
+            get
+            {
+                return _isTriggred;
+            }
+            set
+            {
+                _isTriggred = value;
+            }
+        }
+    }
+}
